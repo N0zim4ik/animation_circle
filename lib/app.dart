de54b4525a6d3fa45e5c75212083489db313d616
N0zim4ik <muxtorovnozimjon06@gmail.com>
2023-10-07 16:23:23 +0500

    animation button

--- lib/app.dart
+++ lib/app.dart
@@ -8,8 +8,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        useMaterial3: true,
+
       ),
       home: const HomePage(),
     );
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,96 +1,102 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  List<double> aligments = [for(double i = -1.0; i <= 1.0; i+= 0.1)i,];
+  // bool a = afalse;
+  double a = 0;
+  double b = 0;
+
+
+  double x = 0;
+  double y = 0;
+  double randomGenerator() {
+    aligments.shuffle();
+    return aligments[0];
+  }
 
   @override
-  void dispose() {
-    emailController.dispose();
-    passwordController.dispose();
-
-    super.dispose();
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 1)).whenComplete(() {
+      x = randomGenerator();
+      y = randomGenerator();
+    });
   }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Form(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  controller: emailController,
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Please enter something!';
-                    } else if (value.length < 6) {
-                      return 'Email length must be greater then 6 characters';
-                    } else if (!value.contains('@')) {
-                      'Email must be contains @';
-                    } else if (!value.contains('.')) {
-                      return 'Email must be contains .';
-                    } else if (value.substring(value.indexOf('.')).length < 2) {
-                      return 'There was a mistake at the and of email';
-                    }
-                  },
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                  ),
+      body: Stack(
+        children: [
+          AnimatedAlign(
+            alignment: Alignment(x, y),
+            duration: const Duration(seconds: 5),
+            onEnd: () {
+              x = randomGenerator();
+              y = randomGenerator();
+              setState(() {
+
+              });
+            },
+            child: GestureDetector(
+              onTapDown: (details) {
+                a = details.globalPosition.dx > 0 ? details.globalPosition.dx : 1;
+                b = details.globalPosition.dy > 0 ? details.globalPosition.dy : 1;
+
+                setState(() {
+
+                });
+              },
+              onTapUp :  (details) {
+                a = details.globalPosition.dx > 0 ? details.globalPosition.dx : 1;
+                b = details.globalPosition.dy > 0 ? details.globalPosition.dy : 1;
+                setState(() {
+
+                });
+              },
+              onPanUpdate: (details) {
+                 a = details.globalPosition.dx > 0 ? details.globalPosition.dx : 1;
+                 b = details.globalPosition.dy > 0 ? details.globalPosition.dy : 1;
+                 setState(() {
+
+                 });
+                print(details.localPosition);
+                // print(details.globalPosition);
+                // print(details.delta);
+              },
+              child: SizedBox(
+                width: 40,
+                height: 40,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(
+                          20,
+                        ),
+                      )),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Please enter something!';
-                    } else if (value.length < 8) {
-                      return 'Email length must be greater then 6 characters';
-                    } else if (value.contains(RegExp(r'[0-9]'))) {
-                      'Password most be number';
-                    } else if (value.contains(RegExp(r'[a-zA-Z]'))) {
-                      return 'Password most be contains latter';
-                    } else if (value.substring(value.indexOf('.')).length < 2) {
-                      return 'Password most be contains latter';
-                    }
-                  },
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                FilledButton(
-                  style: const ButtonStyle(
-                    foregroundColor: MaterialStatePropertyAll(
-                      Colors.black,
-                    ),
-                  ),
-                  onPressed: () {
-                    if (formKey.currentState?.validate() ?? false) {
-                      print('Every ');
-                    }
-                  },
-                  child: const Text('Sign in'),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: a, vertical: b),
+            child: Icon(
+              Icons.touch_app_rounded,
+              color: Colors.red,
+            ),
+          ),
+        ],
       ),
     );
   }
